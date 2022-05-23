5f8fa5f32410d9fbbac1402510d14ac78686dc5a
Pixel Phobic <65147646+PixelPhobicGames@users.noreply.github.com>
2022-05-23 08:30:14 -0700

    Quick Fix

--- Screen.h
+++ Screen.h
@@ -5,8 +5,10 @@
 #define ScreenHeight 32
 
 static int ScreenScale = 6;
+static int XOff = 7;
+static int YOff = 24;
 
-int ClearScreen(unsigned char Color){
+void ClearScreen(unsigned char Color){
     for (int x = 0 ; x <= ScreenWidth; x ++){
         for (int y = 0 ; y <= ScreenHeight; y ++){
             ScreenMemory[x][y] = Color;
