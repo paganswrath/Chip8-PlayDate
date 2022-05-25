1f809bfe711efc2eb97343ca4532dc0bc4c61d90
Pixel Phobic <65147646+PixelPhobicGames@users.noreply.github.com>
2022-05-24 22:14:44 -0700

    ?

--- Build.sh
+++ Build.sh
@@ -3,7 +3,6 @@ make
 cp -r Roms/PONG.bin Game.pdx/
 cp -r Roms/BREAK.bin Game.pdx/
 cp -r Roms/MISSILE.bin Game.pdx/
-cp -r Roms/TestRom.bin Game.pdx/
 cp -r Roms/SPACE.bin Game.pdx/
 zip -r temp.zip Game.pdx/
 rm -r -v Game.pdx/
